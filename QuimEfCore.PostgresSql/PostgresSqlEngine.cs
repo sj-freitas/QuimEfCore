c268de88a36fe78c93290d37797fec40f049b614
Sérgio Freitas <sjcsgfreitas@hotmail.com>
2019-03-05 17:22:07 +0000

    add postgresSQL and sqlServer support

---
+++ QuimEfCore.PostgresSql/PostgresSqlEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuimEfCore.PostgresSql
+{
+    public class PostgresSqlEngine : ActionSqlEngine
+    {
+        public static Action<DbContextOptionsBuilder> Get(string connectionString,
+            Assembly migrationsAssembly = null)
+        {
+            return builder =>
+                builder.UseNpgsql(
+                    connectionString,
+                    options =>
+                    {
+                        if (migrationsAssembly != null)
+                        {
+                            options.MigrationsAssembly(migrationsAssembly
+                                .GetName()
+                                .Name);
+                        }
+                    });
+        }
+
+        public PostgresSqlEngine(string connectionString,
+            Assembly migrationsAssembly = null)
+            : base(Get(connectionString, migrationsAssembly))
+        {
+        }
+    }
+}
---
+++ QuimEfCore.SqlServer/SqlServerEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuimEfCore.SqlServer
+{
+    public class SqlServerEngine : ActionSqlEngine
+    {
+        public static Action<DbContextOptionsBuilder> Get(string connectionString,
+            Assembly migrationsAssembly = null)
+        {
+            return builder =>
+                builder.UseSqlServer(
+                    connectionString,
+                    options =>
+                    {
+                        if (migrationsAssembly != null)
+                        {
+                            options.MigrationsAssembly(migrationsAssembly
+                                .GetName()
+                                .Name);
+                        }
+                    });
+        }
+
+        public SqlServerEngine(string connectionString,
+            Assembly migrationsAssembly = null) 
+            : base(Get(connectionString, migrationsAssembly))
+        {
+        }
+    }
+}
